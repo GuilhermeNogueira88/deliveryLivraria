943d6ff578b71c43a212b24e36994ef0a2c322b1
GUILHERME NOGUEIRA FERREIRA <guilherme.nferreira2@senacsp.edu.br>
2019-11-22 22:13:37 -0300

    22/11

--- src/app/contato/contato/contato.component.ts
+++ src/app/contato/contato/contato.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ContatoService } from '../share/contato.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-contato',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contato.component.css']
 })
 export class ContatoComponent implements OnInit {
+// listacontatos: Observable<any[]>;
+listacontatos: {};
+contatos: {};
 
-  constructor() { }
+  constructor(private contatoService: ContatoService ) { }
 
   ngOnInit() {
+    // this.contatos = this.contatoService.getAll();
+    const subscribe = this.contatoService.getAll().subscribe( (dados:any) =>{
+      this.contatos = dados;
+      const total = dados.length;
+      for(var i=0; i < total; i++) {
+        console.log(dados[i].key);
+        const dsubscribe = this.contatoService.getByKey(dados[i].key).subscribe( (dado:any) =>{
+          dsubscribe.unsubscribe;
+          console.log(dado.key.texto);
+          // this.listacontatos = {
+          //   mensagem: dado.texto,
+          // }
+        });
+      }
+      console.log(this.contatos);
+      //console.log(dados[total].key);
+    });
+
+    // console.log(this.contatos);
+  }
+
+  remover(key: string, filePath: string) {
+    this.contatoService.remove(key, filePath);
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
-import { firebaseconfig } from "src/firebase-config";
+import { firebaseconfig } from 'src/app/firebase-config';
+
 export const environment = {
   production: true,
   firebase: firebaseconfig
